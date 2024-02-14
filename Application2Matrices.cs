bfe5f8e2fa9549ece9b5d4bbece4dc6c99c8508a
Yenooh <128099918+Yenooh@users.noreply.github.com>
2024-02-14 14:39:26 -0500

    Create Application2Matrices.cs

---
+++ Application2Matrices.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Applications2Matrices
+{
+  class Program
+  {
+    public static void printArr(int[,] arr, int n, int m){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                Console.Write(arr[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public static int findNum(int[,] arr, int n, int m){
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(arr[i,j] < 500 || arr[i,j] > 100){
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+    static void Main(string[] args)
+    {
+      Console.Write("Enter N: ");
+      int n = int.Parse(Console.ReadLine());
+      Console.Write("Enter M: ");
+      int m = int.Parse(Console.ReadLine());
+      int[,] arr = new int[n,m];
+      for(int i = 0; i < n; i++){
+          for(int j = 0; j < m; j++){
+              Console.Write("Enter (" + i + "," + j + "):");
+              arr[i,j] = int.Parse(Console.ReadLine());
+          }
+      }
+      printArr(arr,n,m);
+      Console.WriteLine(findNum(arr,n,m));
+    }
+  }
+}
