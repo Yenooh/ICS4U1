209da4eeaec1f1a585dc1074afeff247ab1c5ad1
Yenooh <128099918+Yenooh@users.noreply.github.com>
2024-02-26 20:56:31 -0500

    Update 2DArrays-MatricesAssignment.cs
    completed!!!
    - needs to looking over and finishing touches

--- 2DArrays-MatricesAssignment.cs
+++ 2DArrays-MatricesAssignment.cs
@@ -4,9 +4,102 @@ namespace Applications2MatricesQ3
 {
   class Program
   {
-    public static void generateTranspose(int[,] mat){
-      
+    public static void multiplyMatrix(){
+      Console.Write("Enter rows: ");
+      int r = int.Parse(Console.ReadLine());
+      Console.Write("Enter column: ");
+      int c = int.Parse(Console.ReadLine());
+      int[,] mat1 = new int[r,c];
+      int[,] mat2 = new int[c,r];
+      int[,] final = new int[r,r];
+      Random rnd = new Random();
+      for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+          mat1[i,j] = rnd.Next(10);
+        }
+      }
+      for(int i = 0; i < c; i++){
+        for(int j = 0; j < r; j++){
+          mat2[i,j] = rnd.Next(10);
+        }
+      }
+      for(int i = 0; i < r; i++){
+        for(int j = 0; j < r; j++){
+          int tot = 0;
+          for(int n = 0; n < c; n++){
+            tot += (mat1[i,n]*mat2[n,j]);
+          }
+          final[i,j] = tot;
+        }
+      }
+      for(int i = 0; i < r; i++){
+           for(int j = 0;j < c; j++){
+              Console.Write(mat1[i,j] + " ");
+            }
+            Console.WriteLine();
+          }
+          Console.WriteLine();
+      for(int i = 0; i < c; i++){
+        for(int j = 0;j < r; j++){
+          Console.Write(mat2[i,j] + " ");
+        }
+          Console.WriteLine();
+        }
+      Console.WriteLine();
+      for(int i = 0; i < r; i++){
+        for(int j = 0;j < r; j++){
+          Console.Write(final[i,j] + " ");
+        }
+          Console.WriteLine();
+        }
+      Console.WriteLine();
     }
+    public static void multiplyMatrixByConstant(int[,] mat){  
+      int r = mat.GetLength(0);
+      int c = mat.GetLength(1);
+      int[,] multi = new int[r,c];
+      Console.Write("Enter value to multiply by: ");
+      int m = int.Parse(Console.ReadLine());
+      for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+          multi[i,j] = mat[i,j]*m;
+        }
+      }
+      for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+          Console.Write(multi[i,j] + " ");
+        }
+        Console.WriteLine();
+      }
+      Console.WriteLine();
+    }
+    public static void generateTranspose(int[,] mat){
+      int cnew = mat.GetLength(0);
+      int rnew = mat.GetLength(1);
+      int[,] tran = new int[rnew,cnew];
+      for(int i = 0; i < rnew; i++){
+        for(int j = 0; j < cnew; j++){
+          tran[j,i] = mat[i,j];
+        }
+      }
+      Console.WriteLine("Old Matrix: ");
+      for(int i = 0; i < rnew; i++){
+        for(int j = 0; j < cnew; j++){
+          Console.Write(mat[i,j] + " ");
+        }
+        Console.WriteLine();
+      }
+      Console.WriteLine();
+      Console.WriteLine("Transposed Matrix: ");
+      for(int i = 0; i < rnew; i++){
+        for(int j = 0; j < cnew; j++){
+          Console.Write(tran[i,j] + " ");
+        }
+        Console.WriteLine();
+      }
+      Console.WriteLine();
+    }
+
     public static int[,] generateMatrix(){
       Console.Write("Enter # of rows: ");
       int r = int.Parse(Console.ReadLine());
@@ -43,31 +136,50 @@ namespace Applications2MatricesQ3
     }
     static void Main(string[] args)
     {
-      Console.WriteLine("----------Welcome to the Matrix Program!---------------");
-      Console.WriteLine("Please select one of the following options:");
-      Console.WriteLine();
-      Console.WriteLine("1: The Random Matrix");
-      Console.WriteLine("2: The Transpose Matrix");
-      Console.WriteLine("3: Multiplying a Matrix by a Constant");
-      Console.WriteLine("4: Multiplying Two Matrices");
-      Console.WriteLine("5: Exit");
-      int choice = int.Parse(Console.ReadLine());
-      int[,] mat;
-      switch(choice){
-        case 1:
-        mat = generateMatrix();
-        for(int i = 0; i < mat.GetLength(0); i++){
-          for(int j = 0;j < mat.GetLength(1); j++){
-            Console.Write(mat[i,j] + " ");
+      bool exit = false;
+      
+      int[,] mat = new int[50,60];
+      while(exit == false){
+        Console.WriteLine("----------Welcome to the Matrix Program!---------------");
+        Console.WriteLine("Please select one of the following options:");
+        Console.WriteLine();
+        Console.WriteLine("1: The Random Matrix");
+        Console.WriteLine("2: The Transpose Matrix");
+        Console.WriteLine("3: Multiplying a Matrix by a Constant");
+        Console.WriteLine("4: Multiplying Two Matrices");
+        Console.WriteLine("5: Exit");
+        Console.Write("Select an Option: ");
+        int choice = int.Parse(Console.ReadLine());
+        switch(choice){
+          case 1:
+          mat = generateMatrix();
+          for(int i = 0; i < mat.GetLength(0); i++){
+           for(int j = 0;j < mat.GetLength(1); j++){
+              Console.Write(mat[i,j] + " ");
+            }
+            Console.WriteLine();
           }
           Console.WriteLine();
-        }
-        break;
+          break;
 
-        case 2:
-        generateTranspose(mat);
-        break;
+          case 2:
+            generateTranspose(mat);
+            break;
+
+          case 3:
+            multiplyMatrixByConstant(mat);
+            break;
+          
+          case 4:
+            multiplyMatrix();
+            break;
+
+          case 5:
+            exit = true;
+            break;
       }
+      }
+      
     }
   }
 }
